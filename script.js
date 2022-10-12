bedcf0d855e74c3d7a113b26b75e44c1e6c5a746
Maxim N <mnosov622@gmail.com>
2022-10-12 12:32:08 +0700

    basic functionality

---
+++ script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('.main__form');
+const input = document.querySelector('.main__add-input');
+const list = document.querySelector('.main__list');
+const errorMsg = document.querySelector('.main__error-msg');
+
+form.addEventListener('submit', (e) => handleAdd(e));
+input.addEventListener('keyup', () => removeErrorMessage());
+
+list.style.display = "none";
+
+function handleAdd(e) {
+    e.preventDefault();
+    createItem();
+    input.value = "";
+}
+
+function createItem() {
+    if(input.value.trim() === '') {
+        errorMsg.innerText = "Please write your task";
+    }
+
+    else {
+        list.style.display = "block";
+        const listHolder = document.createElement('ul');
+        const item = document.createElement('li');
+        item.className = "main__list-item";
+        item.style.padding = "10px";
+        item.innerText = input.value;
+        listHolder.appendChild(item);
+        list.append(listHolder);
+    }
+
+    const items = document.querySelectorAll('.main__list-item')
+    
+        items.forEach(item => {
+            item.addEventListener('click', () => {
+                item.classList.add("remove");
+            });
+        }); 
+
+}
+
+function removeErrorMessage() {
+    errorMsg.innerText = "";
+}
