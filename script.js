c5ad1d28ca79f7a8af236ac8ad76f933dfa2e2a5
vishwa <tejapaka08@gmail.com>
2022-10-26 04:16:50 +0530

    added delete task functionality,added mark as done functionality(now, task can be toggled b/w done and undone) , rounded task corners for good ui , made other minor adjustments

--- script.js
+++ script.js
@@ -30,17 +30,27 @@ delBtn.style.display = "none";
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 totalTasks.innerText = tasks.length;
-
 if (tasks.length > 0) {
     delBtn.style.display = "block";
     tasks.forEach((task) => {
-        const addedItem = document.createElement("li");
-        addedItem.className = "main__list-item";
-        addedItem.style.padding = "10px";
-        addedItem.innerText = task;
-        tasksList.appendChild(addedItem);
-        list.append(tasksList);
-        console.log(addedItem);
+        // const addedItem = document.createElement("li");
+        // addedItem.className = "main__list-item";
+        // addedItem.style.padding = "10px";
+        // addedItem.innerText = task;
+        // tasksList.appendChild(addedItem);
+        // list.append(tasksList);
+        // console.log(addedItem);
+        tasksList.innerHTML =
+        `<div class="main__list-item">
+        <div class="task_content">${task}</div>
+        <div>
+            <img class="delete_task" src="./src/img/bin.png">
+            <img class="check hide " src="./src/img/check.png">
+        </div>
+        
+
+    </div>` +tasksList.innerHTML;
+         list.append(tasksList);
     });
 } else {
     list.style.display = "none";
@@ -57,14 +67,15 @@ function createItem() {
         errorMsg.innerText = "Please write your task";
     } else {
         list.style.display = "block";
-        const item = document.createElement("li");
-        item.className = "main__list-item";
-        item.style.padding = "10px";
-        item.innerText = input.value;
-
-        tasksList.prepend(item);
+        tasksList.innerHTML =
+        `<div class="main__list-item">
+            <div class="task_content">${input.value}</div>
+            <div>
+                <img class="delete_task" src="./src/img/bin.png">
+                <img class="check hide " src="./src/img/check.png">
+            </div>
+         </div>` +tasksList.innerHTML;
         list.append(tasksList);
-
         tasks.push(input.value);
         localStorage.setItem("tasks", JSON.stringify(tasks));
         totalTasks.innerText = tasks.length;
@@ -104,12 +115,29 @@ document.addEventListener('keydown', (event) => {
 function markAsComplete () {
     const itemsList = document.querySelectorAll(".main__list-item");
     itemsList.forEach((item) => {
+        console.log();
+        const deleteTask = item.querySelector(".delete_task");
+        deleteTask.addEventListener("click" , ()=>{
+            (deleteTask.parentElement.parentElement.parentElement.removeChild(deleteTask.parentElement.parentElement));
+            let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+            totalTasks.innerText = tasks.length;
+            let t = [];
+            if (tasks.length > 0) {
+                let cmp = item.querySelector(".task_content" ).innerHTML;
+                for(var i = 0 ; i < tasks.length ; i++){
+                    if(cmp === tasks[i]){
+                     
+                    }else{
+                        t.push(tasks[i]);
+                    }
+                }
+            }
+            tasks.push(input.value);
+            localStorage.setItem("tasks", JSON.stringify(t));
+        })
         item.addEventListener("click", () => {
-            item.classList.add("remove");
-            const icon = document.createElement("img");
-            icon.src = "./src/img/check.png";
-            icon.className = "add-icon";
-            item.appendChild(icon);
+            const curr = item.querySelector(".check");
+            curr.classList.contains("hide")?curr.classList.remove("hide"):curr.classList.add("hide");
         });
     });
 }
@@ -143,3 +171,4 @@ function myFunction() {
         darkMode=false;
     }
 }
+
